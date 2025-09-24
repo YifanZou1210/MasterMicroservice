924b30cf10cc42c62617bd0ef36cacd31eb3c311
Yifan Zou <zou.yifa@northeastern.edu>
2025-09-24 14:22:30 -0400

    [fea]Note Generated

--- rest-web-service/src/main/java/com/example/rest_web_service/helloworld/HelloWorld.java
+++ rest-web-service/src/main/java/com/example/rest_web_service/helloworld/HelloWorld.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//rest api
+// @ResponseBody + @Controller
+// 1. 该controller中所有方法return都应该直接作为http response body发送给client而不是解析为View
+// 2. 标识class为spring mvc controller，负责处理http请求并返回view
 @RestController
 public class HelloWorld {
 // http request映射到controller的处理方法,method=..只处理get request,path匹配路径
