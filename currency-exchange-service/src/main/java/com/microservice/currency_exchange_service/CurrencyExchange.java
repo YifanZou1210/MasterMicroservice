892fe0d846464c7808653311c4d3dd8d83e114d8
Yifan Zou <zou.yifa@northeastern.edu>
2025-09-30 00:50:11 -0400

    [Fea] crud of curency-exchange, currency-conversion

--- currency-exchange-service/src/main/java/com/microservice/currency_exchange_service/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/microservice/currency_exchange_service/CurrencyExchange.java
@@ -1,19 +1,33 @@
 package com.microservice.currency_exchange_service;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
+    @Id
     private long id;
+    @Column(name = "currency_from")
     private String from;
-    private String tod;
+    @Column(name = "currency_to")
+    private String to;
+    @Column(name = "conversion_multiple")
     private BigDecimal exchangeRate;
+    @Column(name = "environment")
     private String environment;
 
-    public CurrencyExchange(long id, String from, String tod, BigDecimal exchangeRate) {
+    public CurrencyExchange(long id, String from, String to, BigDecimal exchangeRate) {
         this.id = id;
         this.from = from;
-        this.tod = tod;
+        this.to = to;
         this.exchangeRate = exchangeRate;
+    }
+
+    public CurrencyExchange() {
+        super();
     }
 
     public long getId() {
@@ -32,12 +46,12 @@ public class CurrencyExchange {
         this.from = from;
     }
 
-    public String getTod() {
-        return tod;
+    public String getTo() {
+        return to;
     }
 
-    public void setTod(String tod) {
-        this.tod = tod;
+    public void setTo(String tod) {
+        this.to = to;
     }
 
     public BigDecimal getExchangeRate() {
