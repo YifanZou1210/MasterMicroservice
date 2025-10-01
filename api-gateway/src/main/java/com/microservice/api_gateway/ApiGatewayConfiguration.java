cec80d5bf94e3ff5c1cb8e18cec483e3835a6986
Yifan Zou <zou.yifa@northeastern.edu>
2025-10-01 00:25:29 -0400

    [Fea]api gateway, webflux logging function

---
+++ api-gateway/src/main/java/com/microservice/api_gateway/ApiGatewayConfiguration.java
@@ -0,0 +1,25 @@
+package com.microservice.api_gateway;
+
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfiguration {
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        Function<PredicateSpec, Buildable<Route>> routeFunction = p->p.path("/get")
+                .filters(f->f.addRequestHeader("MyHeader","MyURI"))
+                .uri("http://httpbin.org:80");
+        return builder.routes()
+                .route(p->p.path("/currency-exhcange/**").uri("lb://currency-exchange"))
+                .build();
+    }
+}
